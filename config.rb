d515b90d4131e522e0f1cfd5d4e0eff3143514b1
Jamie Lennox <jamielennox@redhat.com>
2014-01-12 18:59:36 +1000

    Basic Theme install

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/wedding"
+http_images_path = "/wedding/images"
+http_generated_images_path = "/wedding/images"
+http_fonts_path = "/wedding/fonts"
+css_dir = "public/stylesheets/wedding"
 
 # Local development paths
 sass_dir = "sass"
